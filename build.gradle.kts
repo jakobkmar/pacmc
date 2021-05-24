755fc8007a328ec822f73cc1e8a3baed4a833a79
Jakob K <bluefireolymp@gmail.com>
2021-05-24 19:27:50 +0200

    Add ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,10 @@ dependencies {
 
     implementation("com.github.ajalt.clikt:clikt:3.2.0")
     implementation("com.github.ajalt.mordant:mordant:2.0.0-beta1")
+
+    implementation("io.ktor:ktor-client-core:1.5.4")
+    implementation("io.ktor:ktor-client-cio:1.5.4")
+    implementation("io.ktor:ktor-client-serialization:1.5.4")
 }
 
 tasks {
