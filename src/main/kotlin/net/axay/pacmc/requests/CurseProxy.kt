b7583b2e342cf7809aedae8192e54369871178e9
Jakob K <bluefireolymp@gmail.com>
2021-05-24 20:41:28 +0200

    Request GameVersionLatestFile

--- src/main/kotlin/net/axay/pacmc/requests/CurseProxy.kt
+++ src/main/kotlin/net/axay/pacmc/requests/CurseProxy.kt
@@ -9,11 +9,29 @@ object CurseProxy {
         val name: String,
         val authors: List<Author>,
         val summary: String,
+        val gameVersionLatestFiles: List<GameVersionLatestFile>,
         val dateReleased: String,
     ) {
         @Serializable
         data class Author(
             val name: String,
         )
+
+        @Serializable
+        data class GameVersionLatestFile(
+            val gameVersion: String,
+            val projectFileName: String,
+            val fileType: Int,
+        )
+
+        val latestVersion: String
+            get() {
+                val versionString = kotlin.run {
+                    gameVersionLatestFiles.firstOrNull { it.fileType == 1 }
+                        ?: gameVersionLatestFiles.firstOrNull { it.fileType == 2 }
+                        ?: gameVersionLatestFiles.first()
+                }.projectFileName
+                return versionString.removeSuffix(".jar")
+            }
     }
 }
