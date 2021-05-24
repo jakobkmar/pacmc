43ccda47fbea8eed3eedf59b8db0eec6b97c87fd
Jakob K <bluefireolymp@gmail.com>
2021-05-24 18:27:10 +0200

    Add cli dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("com.github.ajalt.clikt:clikt:3.2.0")
+    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta1")
 }
 
 tasks {
