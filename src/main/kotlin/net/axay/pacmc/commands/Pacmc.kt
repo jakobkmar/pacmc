4619a3308a857a0ed72b93124f702986cd2171b9
Jakob K <bluefireolymp@gmail.com>
2021-05-24 21:09:00 +0200

    Update Pacmc.kt

--- src/main/kotlin/net/axay/pacmc/commands/Pacmc.kt
+++ src/main/kotlin/net/axay/pacmc/commands/Pacmc.kt
@@ -5,7 +5,7 @@ import com.github.ajalt.clikt.core.subcommands
 
 object Pacmc : CliktCommand() {
     init {
-        subcommands(Install)
+        subcommands(Install, Search)
     }
 
     override fun run() = Unit
