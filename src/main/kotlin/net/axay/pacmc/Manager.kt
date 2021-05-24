d5a09ac73eff40708ce6a46115faebb7438fd4d5
Jakob K <bluefireolymp@gmail.com>
2021-05-24 20:01:50 +0200

    Update Manager.kt

--- src/main/kotlin/net/axay/pacmc/Manager.kt
+++ src/main/kotlin/net/axay/pacmc/Manager.kt
@@ -1,5 +1,20 @@
 package net.axay.pacmc
 
+import com.github.ajalt.mordant.terminal.Terminal
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
 import net.axay.pacmc.commands.Pacmc
 
 fun main(args: Array<String>) = Pacmc.main(args)
+
+val terminal = Terminal()
+
+val ktorClient by lazy {
+    HttpClient(CIO) {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(Values.json)
+        }
+    }
+}
