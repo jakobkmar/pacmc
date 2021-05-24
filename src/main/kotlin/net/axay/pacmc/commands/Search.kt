06b5aebf99cd4338064c33cc7cc94e7d76384b1e
Jakob K <bluefireolymp@gmail.com>
2021-05-24 21:08:48 +0200

    Update Search.kt

--- src/main/kotlin/net/axay/pacmc/commands/Search.kt
+++ src/main/kotlin/net/axay/pacmc/commands/Search.kt
@@ -2,6 +2,7 @@ package net.axay.pacmc.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextStyles.bold
 import io.ktor.client.request.*
@@ -13,16 +14,25 @@ import net.axay.pacmc.requests.CurseProxy
 import net.axay.pacmc.terminal
 
 object Search : CliktCommand() {
-    private const val proxyApi = "https://addons-ecs.forgesvc.net/api/v2/addon/"
-
     private val searchterm by argument()
+    private val gameversion by option("-g", "--game-version")
+
+    private const val proxyApi = "https://addons-ecs.forgesvc.net/api/v2/addon/"
 
     override fun run() = runBlocking {
         withContext(Values.coroutineScope.coroutineContext) {
-            ktorClient.get<List<CurseProxy.Project>>("${proxyApi}search?gameId=432&sectionId=6&searchFilter=$searchterm")
-        }.forEach {
-            terminal.println("${yellow("curseforge/")}${white(bold(it.name))} by ${it.authors.first().name} ${green(it.latestVersion)}")
-            terminal.println("  ${gray(it.summary)}")
+            ktorClient.get<List<CurseProxy.Project>>("${proxyApi}search") {
+                parameter("gameId", 432)
+                parameter("searchFilter", searchterm)
+                if (gameversion != null) parameter("gameVersion", gameversion)
+            }
+        }.forEach { project ->
+            val repo = yellow("curseforge/")
+            val projectName = white(bold(project.name))
+            val author = "by ${project.authors.first().name}"
+            val version = project.latestVersion?.let { green(it) } ?: red("no file available")
+            terminal.println("$repo$projectName $author $version")
+            terminal.println("  ${gray(project.summary)}")
         }
     }
 }
