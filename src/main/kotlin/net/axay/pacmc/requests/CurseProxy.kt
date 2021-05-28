1059d882812dace0486f3757ad3bf9ed5cbfaee6
Jakob K <bluefireolymp@gmail.com>
2021-05-28 16:13:02 +0200

    Create CurseProxy.kt

---
+++ src/main/kotlin/net/axay/pacmc/requests/CurseProxy.kt
@@ -0,0 +1,27 @@
+package net.axay.pacmc.requests
+
+import io.ktor.client.request.*
+import net.axay.pacmc.ktorClient
+import net.axay.pacmc.requests.data.CurseProxyFile
+import net.axay.pacmc.requests.data.CurseProxyMinecraftVersion
+import net.axay.pacmc.requests.data.CurseProxySearchResult
+
+object CurseProxy {
+    private const val proxyApi = "https://addons-ecs.forgesvc.net/api/v2/"
+
+    suspend fun getMinecraftVersions() =
+        ktorClient.get<List<CurseProxyMinecraftVersion>>("${proxyApi}minecraft/version")
+
+    suspend fun search(searchTerm: String, gameVersion: String?, limit: Int?) =
+        ktorClient.get<List<CurseProxySearchResult>>("${proxyApi}addon/search") {
+            parameter("gameId", 432) // game: minecraft
+            parameter("sectionId", 6) // section: mods
+            parameter("searchFilter", searchTerm)
+            parameter("categoryId", 4780)
+            if (gameVersion != null) parameter("gameVersion", gameVersion)
+            if (limit != null) parameter("pageSize", limit)
+        }
+
+    suspend fun getModFiles(id: Int) =
+        ktorClient.get<List<CurseProxyFile>>("${proxyApi}addon/$id/files")
+}
