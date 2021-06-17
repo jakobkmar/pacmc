44a643ae217e9aa04a6d0da2b10b6a6fa528747d
Jakob K <bluefireolymp@gmail.com>
2021-06-18 01:27:08 +0200

    Update mordant

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     implementation("com.github.ajalt.clikt:clikt:3.2.0")
-    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta1")
+    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta2")
 
     implementation("io.ktor:ktor-client-core:1.6.0")
     implementation("io.ktor:ktor-client-cio:1.6.0")
