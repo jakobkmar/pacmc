705e94cd001dd6121587196621bda5df969e52e6
Jakob K <bluefireolymp@gmail.com>
2021-06-24 17:57:22 +0200

    Add remove command

--- src/main/kotlin/net/axay/pacmc/commands/Pacmc.kt
+++ src/main/kotlin/net/axay/pacmc/commands/Pacmc.kt
@@ -5,7 +5,7 @@ import com.github.ajalt.clikt.core.subcommands
 
 object Pacmc : CliktCommand() {
     init {
-        subcommands(Install, Update, Search, List, Archive, Init)
+        subcommands(Install, Update, Search, List, Remove, Archive, Init)
     }
 
     override fun run() = Unit
--- src/main/kotlin/net/axay/pacmc/commands/Remove.kt
+++ src/main/kotlin/net/axay/pacmc/commands/Remove.kt
@@ -1,15 +1,57 @@
 package net.axay.pacmc.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import kotlinx.dnq.query.filter
+import kotlinx.dnq.query.firstOrNull
+import kotlinx.dnq.query.singleOrNull
+import net.axay.pacmc.storage.Xodus
+import net.axay.pacmc.storage.Xodus.xodus
+import net.axay.pacmc.storage.data.PacmcFile
+import net.axay.pacmc.terminal
+import java.io.File
 
 object Remove : CliktCommand(
     "Removes a minecraft mod"
 ) {
     private val archiveName by option("-a", "--archive").default(".minecraft")
 
-    override fun run() {
-        
+    private val mod by argument()
+
+    override fun run() = xodus {
+        val archive = Xodus.getArchiveOrNull(archiveName) ?: return@xodus
+
+        val inputModId = mod.toIntOrNull()
+        val mod = if (inputModId != null) {
+            archive.mods.filter { it.id eq inputModId }.firstOrNull()
+        } else {
+            kotlin.runCatching {
+                archive.mods.filter { it.name eq mod }.singleOrNull()
+            }.onFailure {
+                terminal.warning("There are multiple mods matching the given name, please specify the ID")
+            }.getOrNull()
+        }
+
+        if (mod == null) {
+            terminal.danger("You don't have any mod with the given name or ID installed.")
+            return@xodus
+        }
+
+        val (modId, modName) = mod
+
+        terminal.println("Deleting all files of that mod...")
+        (File(archive.path).listFiles() ?: emptyArray()).forEach {
+            if (it.name.startsWith("pacmc_") && PacmcFile(it.name).modId == modId) {
+                it.delete()
+            }
+        }
+
+        terminal.println("Deleting mod from archive list...")
+        archive.mods.remove(mod)
+        mod.delete()
+
+        terminal.success("Successfully deleted mod $modName")
     }
 }
