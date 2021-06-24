df800e930c7adb9828715541bdd32afbb16d9c17
Jakob K <bluefireolymp@gmail.com>
2021-06-24 22:43:48 +0200

    Update to Kotlin 1.5.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.serialization") version "1.5.10"
+    kotlin("jvm") version "1.5.20"
+    kotlin("plugin.serialization") version "1.5.20"
     application
 }
 
