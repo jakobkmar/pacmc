674dc6ee05474f284c36a6806b86097562a4474c
Jakob K <bluefireolymp@gmail.com>
2021-07-11 20:32:45 +0200

    Add getArchiveOrWarn extension function

--- src/main/kotlin/net/axay/pacmc/storage/Database.kt
+++ src/main/kotlin/net/axay/pacmc/storage/Database.kt
@@ -1,8 +1,16 @@
 package net.axay.pacmc.storage
 
 import net.axay.pacmc.Values
+import net.axay.pacmc.storage.data.DbArchive
+import net.axay.pacmc.terminal
 import org.kodein.db.DB
+import org.kodein.db.getById
 import org.kodein.db.impl.open
 import java.io.File
 
 val db = DB.open(File(Values.projectDirectories.dataLocalDir, "/db1").canonicalPath)
+
+fun DB.getArchiveOrWarn(name: String) = db.getById<DbArchive>(name).apply {
+    if (this == null)
+        terminal.danger("The given archive '${name}' does not exist!")
+}
