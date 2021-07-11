67094d9719579bb1680736dcb1862526186fc8c3
Jakob K <bluefireolymp@gmail.com>
2021-07-11 21:42:39 +0200

    Use new database in list command

--- src/main/kotlin/net/axay/pacmc/commands/List.kt
+++ src/main/kotlin/net/axay/pacmc/commands/List.kt
@@ -7,13 +7,11 @@ import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextStyles.bold
 import com.github.ajalt.mordant.rendering.TextStyles.underline
-import kotlinx.dnq.query.filter
-import kotlinx.dnq.query.firstOrNull
-import kotlinx.dnq.query.toList
-import net.axay.pacmc.storage.Xodus
-import net.axay.pacmc.storage.data.XdArchive
-import net.axay.pacmc.storage.data.XdMod
+import net.axay.pacmc.storage.data.DbMod
+import net.axay.pacmc.storage.db
+import net.axay.pacmc.storage.getArchiveOrWarn
 import net.axay.pacmc.terminal
+import org.kodein.db.get
 
 object List : CliktCommand(
     "Lists the installed mods"
@@ -22,45 +20,39 @@ object List : CliktCommand(
     private val muteDependencies by option("-m", "--mute", help = "Whether to list mods which are dependencies of other mods").flag()
 
     override fun run() {
-        Xodus.store.transactional {
-            val archive = XdArchive.filter { it.name eq archiveName }.firstOrNull()
-            if (archive == null) {
-                terminal.danger("The given archive '${archiveName}' does not exist!")
-                return@transactional
+        val archive = db.getArchiveOrWarn(archiveName) ?: return
+        val mods = archive.mods.mapNotNull { db[it] }
+
+        fun printMod(mod: DbMod, persistent: Boolean) {
+            val name = white(bold(underline(mod.name)))
+            val id = brightBlue("[${mod.repository}/${mod.modId}]")
+
+            val type = if (!persistent) " ${cyan("(dependency)")}" else ""
+
+            terminal.println(" → $name $id$type")
+            terminal.println("    ${gray(mod.description ?: "no description available")}")
+        }
+
+        mods.apply {
+            val installedMods = filter { it.persistent }
+            val installedDependencies = filter { !it.persistent }
+
+            if (installedMods.isEmpty() && installedDependencies.isEmpty()) {
+                terminal.warning("The archive '$archiveName' is empty!")
+                return
             }
 
-            fun printMod(mod: XdMod, persistent: Boolean) {
-                val name = white(bold(underline(mod.name)))
-                val id = brightBlue("[${mod.repository}/${mod.id}]")
+            terminal.println("The archive '${green(archiveName)}' at ${gray(archive.path)} contains the following mods:")
 
-                val type = if (!persistent) " ${cyan("(dependency)")}" else ""
-
-                terminal.println(" → $name $id$type")
-                terminal.println("    ${gray(mod.description ?: "no description available")}")
+            if (installedMods.isNotEmpty()) {
+                terminal.println()
+                installedMods.forEach { printMod(it, true) }
             }
 
-            archive.mods.apply {
-                val installedMods = filter { it.persistent eq true }.toList()
-                val installedDependencies = filter { it.persistent eq false }.toList()
-
-                if (installedMods.isEmpty() && installedDependencies.isEmpty()) {
-                    terminal.warning("The archive '$archiveName' is empty!")
-                    return@transactional
-                }
-
-                terminal.println("The archive '${green(archiveName)}' at ${gray(archive.path)} contains the following mods:")
-
-                if (installedMods.isNotEmpty()) {
-                    terminal.println()
-                    installedMods.forEach { printMod(it, true) }
-                }
-
-                if (!muteDependencies && installedDependencies.isNotEmpty()) {
-                    terminal.println()
-                    installedDependencies.forEach { printMod(it, false) }
-                }
+            if (!muteDependencies && installedDependencies.isNotEmpty()) {
+                terminal.println()
+                installedDependencies.forEach { printMod(it, false) }
             }
-
         }
     }
 }
