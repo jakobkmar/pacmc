3b99aad34e12054c70f0d975dabb8fb58528d768
Jakob K <bluefireolymp@gmail.com>
2021-07-11 20:59:23 +0200

    Update DbMod.kt

--- src/main/kotlin/net/axay/pacmc/storage/data/DbMod.kt
+++ src/main/kotlin/net/axay/pacmc/storage/data/DbMod.kt
@@ -1,17 +1,16 @@
 package net.axay.pacmc.storage.data
 
 import kotlinx.serialization.Serializable
+import org.kodein.db.model.Id
 import org.kodein.db.model.Indexed
-import org.kodein.db.model.orm.Metadata
 
 @Serializable
 data class DbMod(
+    @Id val uid: String,
     val repository: String,
-    val modid: String,
+    val modId: String,
     val version: String,
     @Indexed("name") val name: String,
     val description: String,
     val persistent: Boolean,
-) : Metadata {
-    override val id get() = listOf(repository, modid)
-}
+)
