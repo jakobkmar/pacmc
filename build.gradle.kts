c6652ba18f1f5acc5ced8ed75372f9f1d5cd6eeb
Jakob K <bluefireolymp@gmail.com>
2021-10-19 22:42:29 +0200

    Update dependencies and Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.5.30"
-    kotlin("plugin.serialization") version "1.5.30"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
     application
 }
 
@@ -22,12 +22,12 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.ajalt.clikt:clikt:3.2.0")
+    implementation("com.github.ajalt.clikt:clikt:3.3.0")
     implementation("com.github.ajalt.mordant:mordant:2.0.0-beta2")
 
-    implementation("io.ktor:ktor-client-core:1.6.3")
-    implementation("io.ktor:ktor-client-cio:1.6.3")
-    implementation("io.ktor:ktor-client-serialization:1.6.3")
+    implementation("io.ktor:ktor-client-core:1.6.4")
+    implementation("io.ktor:ktor-client-cio:1.6.4")
+    implementation("io.ktor:ktor-client-serialization:1.6.4")
 
     implementation("dev.dirs:directories:26")
     implementation("org.apache.commons:commons-text:1.9")
