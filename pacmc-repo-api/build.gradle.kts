fb7eadec9015693ffa8d17e88bd513c4e193cc08
Jakob K <dev@jakobk.de>
2021-12-21 03:59:25 +0100

    Add modrinth models to repo api

---
+++ pacmc-repo-api/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    `kotlin-mp-script`
+    kotlin("plugin.serialization")
+    id("org.openapi.generator") version "5.3.0"
+}
+
+kotlin {
+    jvm()
+
+    sourceSets {
+        named("commonMain") {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+            }
+        }
+    }
+}
+
+openApiGenerate {
+    generatorName.set("kotlin")
+    library.set("multiplatform")
+    inputSpec.set("$projectDir/modrinth-openapi.yaml")
+    outputDir.set("$buildDir/generated/modrinth-api")
+    modelPackage.set("net.axay.pacmc.repoapi.modrinth.model")
+}
+
+tasks {
+    this.openApiGenerate {
+        doFirst {
+            println("Downloading Modrinth OpenAPI file...")
+            val openApiFile = file("$projectDir/modrinth-openapi.yaml")
+            uri("https://docs.modrinth.com/openapi.yaml").toURL().openStream().use {
+                it.copyTo(openApiFile.outputStream())
+            }
+            println("Finished downloading Modrinth OpenAPI file.")
+        }
+        doLast {
+            file("$buildDir/generated/modrinth-api/src/commonMain/kotlin/net/axay")
+                .copyRecursively(file("$projectDir/src/commonMain/kotlin/net/axay"), overwrite = true)
+        }
+    }
+}
