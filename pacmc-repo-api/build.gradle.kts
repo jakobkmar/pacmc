e5c581b7bc337c20099341cade4a982d15416c0a
Jakob K <dev@jakobk.de>
2021-12-23 02:06:02 +0100

    Remove explicit kx.serialization dependency from repo-api

--- pacmc-repo-api/build.gradle.kts
+++ pacmc-repo-api/build.gradle.kts
@@ -10,7 +10,6 @@ kotlin {
     sourceSets {
         named("commonMain") {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
 
                 api("io.ktor:ktor-client-core:1.6.7")
                 api("io.ktor:ktor-client-serialization:1.6.7")
