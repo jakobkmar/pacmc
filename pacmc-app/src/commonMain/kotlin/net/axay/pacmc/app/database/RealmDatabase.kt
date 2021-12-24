efa3171ffabdcbb5f67022976f06be566557e2b8
Jakob K <dev@jakobk.de>
2021-12-24 01:33:11 +0100

    Add realm database object

---
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/database/RealmDatabase.kt
@@ -0,0 +1,15 @@
+package net.axay.pacmc.app.database
+
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import net.axay.pacmc.app.Environment
+
+object RealmDatabase {
+    val realm = kotlin.run {
+        val config = RealmConfiguration.Builder()
+            .path(Environment.dataLocalDir.resolve("/db2").toString())
+            .schema()
+            .build()
+        Realm.open(config)
+    }
+}
