8d95287a1fac0a4d7ce7ec9a504af1ae3d3f5849
Jakob K <bluefireolymp@gmail.com>
2022-01-03 04:33:47 +0100

    Make realm a global val

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/database/RealmDatabase.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/database/RealmDatabase.kt
@@ -4,12 +4,10 @@ import io.realm.Realm
 import io.realm.RealmConfiguration
 import net.axay.pacmc.app.Environment
 
-object RealmDatabase {
-    val realm = kotlin.run {
-        val config = RealmConfiguration.Builder()
-            .path(Environment.dataLocalDir.resolve("/db2").toString())
-            .schema()
-            .build()
-        Realm.open(config)
-    }
+val realm = kotlin.run {
+    val config = RealmConfiguration.Builder()
+        .path(Environment.dataLocalDir.resolve("/db2").toString())
+        .schema()
+        .build()
+    Realm.open(config)
 }
