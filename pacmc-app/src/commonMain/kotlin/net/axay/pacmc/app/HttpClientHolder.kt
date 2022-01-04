277190923ce64d6fca16addc41bd64b76fb4c89e
Jakob K <bluefireolymp@gmail.com>
2022-01-04 17:19:22 +0100

    Explicitly use CIO

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/HttpClientHolder.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/HttpClientHolder.kt
@@ -1,10 +1,11 @@
 package net.axay.pacmc.app
 
 import io.ktor.client.*
+import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 
-val ktorClient = HttpClient {
+val ktorClient = HttpClient(CIO) {
     install(JsonFeature) {
         serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
             ignoreUnknownKeys = true
