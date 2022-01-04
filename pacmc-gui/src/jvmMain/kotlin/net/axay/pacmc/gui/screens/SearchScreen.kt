58d00e34e1673ec3781e1155babb92ce27200259
Jakob K <bluefireolymp@gmail.com>
2022-01-04 17:24:02 +0100

    Handle failed search requests

--- pacmc-gui/src/jvmMain/kotlin/net/axay/pacmc/gui/screens/SearchScreen.kt
+++ pacmc-gui/src/jvmMain/kotlin/net/axay/pacmc/gui/screens/SearchScreen.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -22,8 +24,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.*
+import co.touchlab.kermit.Logger
 import compose.icons.TablerIcons
 import compose.icons.tablericons.Download
 import kotlinx.coroutines.delay
@@ -33,57 +37,113 @@ import net.axay.pacmc.app.repoapi.RepositoryApi
 import net.axay.pacmc.app.repoapi.model.CommonProjectInfo
 import net.axay.pacmc.gui.cache.producePainterCached
 
+private sealed interface SearchResponse
+
+private class SearchResponseSuccess(
+    val results: List<CommonProjectInfo>,
+) : SearchResponse
+
+private class SearchResponseFailure(
+    val reason: String,
+) : SearchResponse
+
 @OptIn(ExperimentalFoundationApi::class, ExperimentalUnitApi::class)
 @Composable
-fun SearchScreen() = Column {
+fun SearchScreen() = Box {
     val searchScope = rememberCoroutineScope()
     var searchTerm by remember { mutableStateOf("") }
-    val searchResults = remember { mutableStateListOf<CommonProjectInfo>() }
+    var loading by remember { mutableStateOf(false) }
+    var searchResponse by remember { mutableStateOf<SearchResponse>(SearchResponseSuccess(emptyList())) }
 
-    Row(
-        Modifier
-            .fillMaxWidth().padding(20.dp).height(40.dp)
-            .background(Color.White, RoundedCornerShape(10.dp)),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(Icons.Default.Search, "Search", Modifier.padding(start = 8.dp).padding(vertical = 4.dp))
-        Box(contentAlignment = Alignment.CenterStart) {
-            BasicTextField(
-                searchTerm,
-                onValueChange = {
-                    searchTerm = it
-                    searchScope.launch {
-                        delay(200)
-                        if (searchTerm != it) return@launch
+    fun fetchResults(typing: Boolean) {
+        val fetchTerm = searchTerm
 
-                        val projects = RepositoryApi.search(it, Repository.MODRINTH)
-                        if (searchTerm == it) {
-                            searchResults.clear()
-                            searchResults.addAll(projects)
-                        }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth().padding(start = 4.dp, end = 8.dp).padding(vertical = 4.dp),
-                textStyle = TextStyle(fontSize = TextUnit(18f, TextUnitType.Sp)),
-                maxLines = 1,
-            )
-            if (searchTerm.isEmpty()) {
-                Text("Search...", Modifier.padding(start = 4.dp))
+        loading = true
+        searchScope.launch {
+            if (typing) {
+                delay(200)
+                if (searchTerm != fetchTerm) return@launch
+            }
+
+            val result = kotlin.runCatching {
+                RepositoryApi.search(fetchTerm, Repository.MODRINTH)
+            }
+            if (searchTerm == fetchTerm) {
+                loading = false
+                result.onFailure { exc ->
+                    Logger.w("Failed to fetch search results for '${fetchTerm}' (${exc.message})")
+                    searchResponse = SearchResponseFailure(exc.message ?: "unknown reason")
+                }.onSuccess {
+                    searchResponse = SearchResponseSuccess(it)
+                }
             }
         }
     }
 
-    val listState = rememberLazyListState()
+    if (loading) {
+        LinearProgressIndicator(Modifier.fillMaxWidth().height(3.dp), color = Color.DarkGray, )
+    }
 
-    LazyVerticalGrid(
-        state = listState,
-        cells = GridCells.Adaptive(500.dp),
-        contentPadding = PaddingValues(start = 20.dp, end = 20.dp, bottom = 20.dp),
-        horizontalArrangement = Arrangement.spacedBy(15.dp),
-        verticalArrangement = Arrangement.spacedBy(15.dp),
-    ) {
-        items(searchResults) {
-            ProjectItem(it)
+    Column {
+        Row(
+            Modifier
+                .fillMaxWidth().padding(20.dp).height(40.dp)
+                .background(Color.White, RoundedCornerShape(10.dp)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(Icons.Default.Search, "Search", Modifier.padding(start = 8.dp).padding(vertical = 4.dp))
+            Box(contentAlignment = Alignment.CenterStart) {
+                BasicTextField(
+                    searchTerm,
+                    onValueChange = {
+                        searchTerm = it
+                        fetchResults(true)
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(start = 4.dp, end = 8.dp).padding(vertical = 4.dp),
+                    textStyle = TextStyle(fontSize = TextUnit(18f, TextUnitType.Sp)),
+                    maxLines = 1,
+                )
+                if (searchTerm.isEmpty()) {
+                    Text("Search...", Modifier.padding(start = 4.dp))
+                }
+            }
+        }
+
+        val listState = rememberLazyListState()
+
+        when (val currentResponse = searchResponse) {
+            is SearchResponseSuccess -> {
+                LazyVerticalGrid(
+                    state = listState,
+                    cells = GridCells.Adaptive(500.dp),
+                    contentPadding = PaddingValues(start = 20.dp, end = 20.dp, bottom = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(15.dp),
+                    verticalArrangement = Arrangement.spacedBy(15.dp),
+                ) {
+                    items(currentResponse.results) {
+                        ProjectItem(it)
+                    }
+                }
+            }
+            is SearchResponseFailure -> {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        buildString {
+                            appendLine("Did not receive a valid response for '${searchTerm}'")
+                            appendLine("Reason: ${currentResponse.reason}")
+                        },
+                        textAlign = TextAlign.Center
+                    )
+                    Button(
+                        onClick = { fetchResults(false) },
+                    ) {
+                        Text("Try again")
+                    }
+                }
+            }
         }
     }
 }
