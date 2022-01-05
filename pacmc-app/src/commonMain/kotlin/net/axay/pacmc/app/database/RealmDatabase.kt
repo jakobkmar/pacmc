ad96a5041db5a59f2dfdcd2e11925010a3e772d9
Jakob K <bluefireolymp@gmail.com>
2022-01-05 20:31:22 +0100

    Remove redundant qualifier

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/database/RealmDatabase.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/database/RealmDatabase.kt
@@ -4,7 +4,7 @@ import io.realm.Realm
 import io.realm.RealmConfiguration
 import net.axay.pacmc.app.Environment
 
-val realm = kotlin.run {
+val realm = run {
     val config = RealmConfiguration.Builder()
         .path(Environment.dataLocalDir.resolve("/db2").toString())
         .schema()
