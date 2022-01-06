422dbf47c3808ae83dd4c98d2ed02a9004762593
Jakob K <bluefireolymp@gmail.com>
2022-01-06 02:41:11 +0100

    Add archive database methods

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/features/Archive.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/features/Archive.kt
@@ -7,35 +7,33 @@ import io.ktor.client.statement.*
 import io.realm.objects
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.toList
 import net.axay.pacmc.app.Environment
-import net.axay.pacmc.app.data.MinecraftVersion
 import net.axay.pacmc.app.data.ModFile
 import net.axay.pacmc.app.data.ModId
-import net.axay.pacmc.app.data.ModLoader
 import net.axay.pacmc.app.database.model.DbArchive
 import net.axay.pacmc.app.database.realm
 import net.axay.pacmc.app.ktorClient
 import net.axay.pacmc.app.repoapi.RepositoryApi
 import okio.Path.Companion.toPath
 
-class Archive(val name: String) {
+class Archive(private val name: String) {
+    companion object {
+        suspend fun getArchives() = realm.objects<DbArchive>().asFlow().toList()
+
+        suspend fun create(dbArchive: DbArchive) {
+            realm.write {
+                copyToRealm(dbArchive)
+            }
+        }
+    }
+
     private lateinit var dbArchive: DbArchive
 
     private fun updateFromDb(): DbArchive? {
         dbArchive = realm.objects<DbArchive>().query("name == $0 LIMIT(1)", name).firstOrNull() ?: return null
         return dbArchive
-    }
-
-    suspend fun create(minecraftVersion: MinecraftVersion, loader: ModLoader) {
-        val archive = DbArchive().apply {
-            this.name = this@Archive.name
-            this.minecraftVersion = minecraftVersion.toString()
-            this.loader = loader
-        }
-
-        realm.write {
-            copyToRealm(archive)
-        }
     }
 
     suspend fun install(
@@ -49,7 +47,7 @@ class Archive(val name: String) {
         val projectVersionsDeferred = async {
             RepositoryApi.getProjectVersions(
                 modId,
-                listOf(dbArchive.loader),
+                listOf(dbArchive.readLoader()),
                 listOf(dbArchive.readMinecraftVersion())
             )
         }
