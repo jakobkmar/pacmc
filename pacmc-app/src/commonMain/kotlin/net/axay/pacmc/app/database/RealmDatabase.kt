fd38231f96923e99569bcf2cfc4e371fdfa9ad09
Jakob K <bluefireolymp@gmail.com>
2022-01-06 02:41:31 +0100

    Create db folder and init schemas

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/database/RealmDatabase.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/database/RealmDatabase.kt
@@ -3,11 +3,17 @@ package net.axay.pacmc.app.database
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import net.axay.pacmc.app.Environment
+import net.axay.pacmc.app.database.model.DbArchive
+import net.axay.pacmc.app.database.model.DbInstalledProject
 
 val realm = run {
     val config = RealmConfiguration.Builder()
-        .path(Environment.dataLocalDir.resolve("/db2").toString())
-        .schema()
+        .path(
+            Environment.dataLocalDir.resolve("db2/pacmc_db").also {
+                Environment.fileSystem.createDirectories(it.parent!!)
+            }.toString()
+        )
+        .schema(DbArchive::class, DbInstalledProject::class)
         .build()
     Realm.open(config)
 }
