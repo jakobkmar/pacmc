2c15fd81d493da610a7ede70b794a9c7078e9cc1
Jakob K <bluefireolymp@gmail.com>
2022-01-13 00:38:59 +0100

    Register IdKotlinXSerializationModule

--- pacmc-server/src/jvmMain/kotlin/net/axay/pacmc/server/ServerApplication.kt
+++ pacmc-server/src/jvmMain/kotlin/net/axay/pacmc/server/ServerApplication.kt
@@ -6,16 +6,20 @@ import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.serialization.json.Json
 import net.axay.pacmc.server.feeds.MinecraftFeedHandler
 import net.axay.pacmc.server.routes.routeIndex
 import net.axay.pacmc.server.routes.routeNews
+import org.litote.kmongo.id.serialization.IdKotlinXSerializationModule
 
 fun main() {
     MinecraftFeedHandler().monitor()
 
     embeddedServer(Netty, port = 8080) {
         install(ContentNegotiation) {
-            json()
+            json(Json {
+                serializersModule = IdKotlinXSerializationModule
+            })
         }
 
         routing {
