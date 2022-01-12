18266eac821971d2028e1829e15a3d08db7ba722
Jakob K <bluefireolymp@gmail.com>
2022-01-13 00:38:47 +0100

    Implement specific article request for news route

--- pacmc-server/src/jvmMain/kotlin/net/axay/pacmc/server/routes/News.kt
+++ pacmc-server/src/jvmMain/kotlin/net/axay/pacmc/server/routes/News.kt
@@ -1,36 +1,59 @@
 package net.axay.pacmc.server.routes
 
+import com.mongodb.client.model.Filters
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import kotlinx.datetime.Instant
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import net.axay.pacmc.server.database.db
 import net.axay.pacmc.server.feeds.MinecraftArticle
+import org.bson.types.ObjectId
 import org.litote.kmongo.*
 
 fun Routing.routeNews() {
-    get("/news/minecraft") {
-        val params = call.request.queryParameters
-        val query = params["query"]
+    route("/news/minecraft") {
+        get {
+            val params = call.request.queryParameters
+            val query = params["query"]
 
-        val pipeline = buildList {
-            if (query != null) {
-                add(match(text(query)))
-                add(sort(MinecraftArticle::title.sortByMetaTextScore()))
-            } else {
-                add(sort(descending(MinecraftArticle::datePublished)))
+            val pipeline = buildList {
+                if (query != null) {
+                    add(match(text(query)))
+                    add(sort(MinecraftArticle::title.sortByMetaTextScore()))
+                } else {
+                    add(sort(descending(MinecraftArticle::datePublished)))
+                }
+                add(project(exclude(MinecraftArticle::contentHtml, MinecraftArticle::contentJson)))
             }
-            add(project(exclude(MinecraftArticle::contentHtml, MinecraftArticle::contentJson)))
+
+            val articles = db.minecraftFeed.aggregate<SearchResult>(pipeline)
+            call.respond(articles.toList())
         }
 
-        val articles = db.minecraftFeed.aggregate<SearchResult>(pipeline)
-        call.respond(articles.toList())
+        get("/{id}") {
+            val params = call.parameters
+            val id = params["id"]
+
+            if (id != null) {
+                val feed = db.minecraftFeed.findOne(Filters.eq(ObjectId(id)))
+                if (feed != null) {
+                    call.respond(feed)
+                    return@get
+                }
+            }
+
+            call.response.status(HttpStatusCode.NotFound)
+        }
     }
 }
 
 @Serializable
 private data class SearchResult(
+    @Contextual @SerialName("_id") val id: Id<SearchResult>,
     val url: String,
     val datePublished: Instant,
     val category: String?,
