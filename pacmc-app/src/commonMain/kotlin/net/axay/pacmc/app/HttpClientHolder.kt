2425ebb707f55f0b325377d20f8ab1ae24d9f2d6
Jakob K <bluefireolymp@gmail.com>
2022-01-25 17:42:03 +0100

    Add downloadFile function for ktor client (adds streaming)

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/HttpClientHolder.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/HttpClientHolder.kt
@@ -1,10 +1,17 @@
 package net.axay.pacmc.app
 
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.client.utils.*
+import io.ktor.utils.io.*
+import io.ktor.utils.io.core.*
+import okio.Path
 
 val ktorClient = HttpClient(CIO) {
     install(JsonFeature) {
@@ -12,6 +19,36 @@ val ktorClient = HttpClient(CIO) {
             ignoreUnknownKeys = true
         })
     }
+}
 
-    BrowserUserAgent()
+suspend inline fun HttpClient.downloadFile(
+    url: String,
+    path: Path,
+    crossinline downloadProgress: (Double) -> Unit = {},
+    builder: HttpRequestBuilder.() -> Unit = {},
+) = get<HttpStatement>(url) {
+    builder(this)
+    onDownload { bytesSentTotal, contentLength ->
+        downloadProgress(bytesSentTotal.toDouble() / contentLength.toDouble())
+    }
+}.execute { response ->
+    val channel = response.receive<ByteReadChannel>()
+
+    val partPath = path.parent!!.resolve(path.name + ".part")
+
+    path.parent?.let(Environment.fileSystem::createDirectories)
+
+    try {
+        Environment.fileSystem.write(partPath) {
+            while (!channel.isClosedForRead) {
+                val packet = channel.readRemaining(DEFAULT_HTTP_BUFFER_SIZE.toLong())
+                while (!packet.isEmpty) {
+                    write(packet.readBytes())
+                }
+            }
+        }
+        Environment.fileSystem.atomicMove(partPath, path)
+    } finally {
+        Environment.fileSystem.delete(partPath, mustExist = false)
+    }
 }
