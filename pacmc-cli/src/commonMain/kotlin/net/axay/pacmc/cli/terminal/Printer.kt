634d2b52a5103422c2a233d5122bff0e77a525bc
Jakob K <dev@jakobk.de>
2022-03-28 21:43:20 +0200

    Create new printProject function

---
+++ pacmc-cli/src/commonMain/kotlin/net/axay/pacmc/cli/terminal/Printer.kt
@@ -0,0 +1,22 @@
+package net.axay.pacmc.cli.terminal
+
+import com.github.ajalt.mordant.rendering.TextColors
+import com.github.ajalt.mordant.rendering.TextStyles
+import com.github.ajalt.mordant.terminal.Terminal
+import net.axay.pacmc.app.data.Repository
+import net.axay.pacmc.app.repoapi.model.CommonProjectInfo
+
+private val Repository.textColor
+    get() = when (this) {
+        Repository.MODRINTH -> TextColors.brightGreen
+        Repository.CURSEFORGE -> TextColors.yellow
+    }
+
+fun Terminal.printProject(project: CommonProjectInfo) = println(buildString {
+    append(project.id.repository.run { textColor(displayName.lowercase() + "/") })
+    append(TextColors.white(TextStyles.bold(TextStyles.underline(project.slug))))
+    project.latestVersion?.let { append(" ${TextColors.brightCyan(it.toString())}") }
+    append(" ${TextStyles.italic("by")} ${project.author}")
+    appendLine()
+    append("  ${TextColors.gray(project.description)}")
+})
