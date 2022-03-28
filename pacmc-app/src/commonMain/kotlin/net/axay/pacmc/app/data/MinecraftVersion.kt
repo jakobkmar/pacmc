6815d6726988d53cbb8e544b14a6c4c6f708f242
Jakob K <dev@jakobk.de>
2022-03-28 21:41:00 +0200

    Make MinecraftVersion comparable

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/data/MinecraftVersion.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/data/MinecraftVersion.kt
@@ -4,10 +4,21 @@ data class MinecraftVersion(
     val bigMajor: Int,
     val major: Int,
     val minor: Int,
-) {
+) : Comparable<MinecraftVersion> {
+
     override fun toString() = when (minor) {
         0 -> "${bigMajor}.${major}"
         else -> "${bigMajor}.${major}.${minor}"
+    }
+
+    override fun compareTo(other: MinecraftVersion): Int {
+        val bigDiff = this.bigMajor.compareTo(other.bigMajor)
+        if (bigDiff != 0) return bigDiff
+        val diff = this.major.compareTo(other.major)
+        if (diff != 0) return diff
+        val minorDiff = this.minor.compareTo(other.minor)
+        if (minorDiff != 0) return minorDiff
+        return 0
     }
 
     companion object {
