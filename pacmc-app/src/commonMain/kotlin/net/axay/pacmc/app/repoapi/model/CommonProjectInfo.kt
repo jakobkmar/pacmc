17b823becad87159d3dcf1410bf6e225f6d612c4
Jakob K <dev@jakobk.de>
2022-03-28 21:41:28 +0200

    Add latest mc version property to project info

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/repoapi/model/CommonProjectInfo.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/repoapi/model/CommonProjectInfo.kt
@@ -1,24 +1,27 @@
 package net.axay.pacmc.app.repoapi.model
 
+import net.axay.pacmc.app.data.MinecraftVersion
 import net.axay.pacmc.app.data.ModId
 import net.axay.pacmc.app.data.Repository
 
 data class CommonProjectInfo(
     val id: ModId,
-    val slug: String?,
+    val slug: String,
     val name: String,
     val author: String,
     val description: String,
     val iconUrl: String?,
+    val latestVersion: MinecraftVersion?,
 ) {
     companion object {
         fun fromModrinthProjectResult(projectResult: net.axay.pacmc.repoapi.modrinth.model.ProjectResult) = CommonProjectInfo(
             id = ModId(Repository.MODRINTH, projectResult.projectId),
-            slug = projectResult.slug,
+            slug = projectResult.slug.toString(), // TODO slugs actually shouldn't be but there are old mods
             name = projectResult.title!!,
             author = projectResult.author,
             description = projectResult.description!!,
             iconUrl = projectResult.iconUrl,
+            latestVersion = projectResult.versions.mapNotNull { MinecraftVersion.fromString(it) }.maxOrNull(),
         )
     }
 }
