9a0386a78d8211ac92cfada6dac8b8f6e8971e93
Jakob K <dev@jakobk.de>
2022-03-30 18:06:00 +0200

    Fix jvm application not exiting due to realm

--- pacmc-cli/src/jvmMain/kotlin/net/axay/pacmc/cli/ApplicationJvm.kt
+++ pacmc-cli/src/jvmMain/kotlin/net/axay/pacmc/cli/ApplicationJvm.kt
@@ -1,3 +1,8 @@
 package net.axay.pacmc.cli
 
-actual suspend fun main(args: Array<String>) = runCli(args)
+import kotlin.system.exitProcess
+
+actual suspend fun main(args: Array<String>) {
+    runCli(args)
+    exitProcess(1) // TODO this shouldn't be necessary but realm currently does not close correctly
+}
