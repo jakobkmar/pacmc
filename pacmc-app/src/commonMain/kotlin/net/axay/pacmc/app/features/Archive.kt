5f8136699b7d5027b53486f8343071bfd4b4659d
Jakob K <dev@jakobk.de>
2022-03-30 03:48:36 +0200

    Add resolve function to archive class

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/features/Archive.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/features/Archive.kt
@@ -1,20 +1,22 @@
 package net.axay.pacmc.app.features
 
-import io.ktor.client.call.*
-import io.ktor.client.features.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.realm.query
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
-import net.axay.pacmc.app.Environment
+import net.axay.pacmc.app.data.MinecraftVersion
 import net.axay.pacmc.app.data.ModFile
 import net.axay.pacmc.app.data.ModId
+import net.axay.pacmc.app.data.ModSlug
 import net.axay.pacmc.app.database.model.DbArchive
+import net.axay.pacmc.app.database.model.DbInstalledProject
 import net.axay.pacmc.app.database.realm
+import net.axay.pacmc.app.downloadFile
 import net.axay.pacmc.app.ktorClient
 import net.axay.pacmc.app.repoapi.RepositoryApi
+import net.axay.pacmc.app.repoapi.model.CommonProjectVersion
+import net.axay.pacmc.app.utils.pmap
 import okio.Path.Companion.toPath
+import kotlin.math.absoluteValue
 
 class Archive(private val name: String) {
     companion object {
@@ -33,6 +35,80 @@ class Archive(private val name: String) {
         dbArchive = realm.query<DbArchive>("name == $0", name).limit(1).first().find() ?: return null
         return dbArchive
     }
+
+    class ResolveResult(
+        val versions: List<CommonProjectVersion>,
+        val dependencyVersions: List<CommonProjectVersion>,
+    )
+
+    suspend fun resolve(modSlugs: List<ModSlug>): ResolveResult {
+        updateFromDb()
+
+        val modIds = modSlugs.pmap { RepositoryApi.getBasicProjectInfo(it)?.id }.filterNotNull().toSet()
+
+        val loader = dbArchive.readLoader()
+        val minecraftVersion = dbArchive.readMinecraftVersion()
+
+        suspend fun resolveTransitively(
+            modIds: Set<ModId>,
+            alreadyResolved: MutableSet<ModId>,
+        ): List<CommonProjectVersion> {
+
+            val versions = modIds.pmap { modId ->
+                if (modId in alreadyResolved) return@pmap null
+
+                val projectVersions = RepositoryApi.getProjectVersions(modId, listOf(loader), listOf(minecraftVersion))
+                projectVersions?.findBest(minecraftVersion)
+            }.filterNotNull()
+
+            val dependencies = versions.pmap { version ->
+                version.dependencies.pmap {
+                    when (it) {
+                        is CommonProjectVersion.Dependency.ProjectDependency -> it.id
+                        is CommonProjectVersion.Dependency.VersionDependency -> RepositoryApi.getProjectVersion(it.id, version.modId.repository)?.modId
+                    }
+                }.filterNotNull()
+            }.flatten().toSet()
+
+            if (dependencies.isEmpty()) return versions
+
+            alreadyResolved += versions.map { it.modId }
+            return versions + resolveTransitively(dependencies, alreadyResolved)
+        }
+
+        val versions = mutableListOf<CommonProjectVersion>()
+        val dependencyVersions = mutableListOf<CommonProjectVersion>()
+
+        resolveTransitively(modIds, mutableSetOf()).forEach {
+            (if (it.modId in modIds) versions else dependencyVersions) += it
+        }
+
+        return ResolveResult(versions, dependencyVersions)
+    }
+
+    private fun List<CommonProjectVersion>.findBest(desiredVersion: MinecraftVersion): CommonProjectVersion? =
+        fold<CommonProjectVersion, Pair<CommonProjectVersion, Int>?>(null) { acc, projectVersion ->
+            val distance = projectVersion.gameVersions
+                .mapNotNull { it.minorDistance(desiredVersion) }
+                .minOrNull()
+
+            when {
+                // the major version does not fit
+                distance == null -> acc
+                // the previous version does not fit but this one does
+                acc == null -> projectVersion to distance
+                // prefer the file closer to the desired version
+                distance.absoluteValue < acc.second.absoluteValue -> projectVersion to distance
+                distance.absoluteValue > acc.second.absoluteValue -> acc
+                // now both files are equally close to the desired version
+                // prefer the one which supports the more recent game version
+                distance > 0 && acc.second < 0 -> projectVersion to distance
+                distance < 0 && acc.second > 0 -> acc
+                // just prefer the newer file
+                projectVersion.datePublished > acc.first.datePublished -> projectVersion to distance
+                else -> acc
+            }
+        }?.first
 
     suspend fun install(
         modId: ModId,
@@ -57,21 +133,16 @@ class Archive(private val name: String) {
 
         val downloadFile = version.files.firstOrNull { it.primary } ?: return@scope
 
-        val fileBytes = ktorClient.get<HttpResponse>(downloadFile.url) {
-            onDownload { bytesSentTotal, contentLength ->
-                downloadProgress(bytesSentTotal.toDouble() / contentLength.toDouble())
-            }
-        }.receive<ByteArray>()
+        val fileName = ModFile(modId.repository.shortForm, projectInfoDeferred.await()?.slug?.slug, modId.id).fileName
 
-        val fileName = ModFile(modId.repository.shortForm, projectInfoDeferred.await()?.slug, modId.id).fileName
+        ktorClient.downloadFile(
+            downloadFile.url,
+            dbArchive.path.toPath().resolve(fileName),
+            downloadProgress
+        )
 
-        Environment.fileSystem.write(dbArchive.path.toPath().resolve(fileName)) {
-            write(fileBytes)
-        }
-
-        // TODO: add mod to list of installed
-        dbArchive.installed
         realm.write {
+            dbArchive.installed.add(DbInstalledProject(modId.id))
             copyToRealm(dbArchive)
         }
     }
