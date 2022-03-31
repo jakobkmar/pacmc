63bb7ad7c89a7e5215a27b59122ad1dc8cb71506
Jakob K <dev@jakobk.de>
2022-03-31 04:49:50 +0200

    Add try catch finally around jvm cli execution

--- pacmc-cli/src/jvmMain/kotlin/net/axay/pacmc/cli/ApplicationJvm.kt
+++ pacmc-cli/src/jvmMain/kotlin/net/axay/pacmc/cli/ApplicationJvm.kt
@@ -3,6 +3,11 @@ package net.axay.pacmc.cli
 import kotlin.system.exitProcess
 
 actual suspend fun main(args: Array<String>) {
-    runCli(args)
-    exitProcess(1) // TODO this shouldn't be necessary but realm currently does not close correctly
+    try {
+        runCli(args)
+    } catch (exc: Exception) {
+        exc.printStackTrace()
+    } finally {
+        exitProcess(1) // TODO this shouldn't be necessary but realm currently does not close correctly
+    }
 }
