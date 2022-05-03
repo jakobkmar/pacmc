6d25344bfc36dbb3878ac2bce70f4e03bc16411a
Jakob K <dev@jakobk.de>
2022-05-03 19:27:41 +0200

    Use handleTransaction for update command

--- pacmc-cli/src/commonMain/kotlin/net/axay/pacmc/cli/commands/UpdateCommand.kt
+++ pacmc-cli/src/commonMain/kotlin/net/axay/pacmc/cli/commands/UpdateCommand.kt
@@ -2,12 +2,10 @@ package net.axay.pacmc.cli.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.mordant.rendering.TextColors
 import net.axay.pacmc.app.features.Archive
-import net.axay.pacmc.app.utils.OperatingSystem
 import net.axay.pacmc.cli.launchJob
 import net.axay.pacmc.cli.terminal
-import net.axay.pacmc.cli.terminal.optimalTerminalString
+import net.axay.pacmc.cli.terminal.handleTransaction
 
 class UpdateCommand : CliktCommand(
     name = "update",
@@ -22,23 +20,12 @@ class UpdateCommand : CliktCommand(
         terminal.println("Resolving updates for the given archive '$archiveName'...")
         val archive = Archive(archiveName!!)
 
-        val updateResult = archive.resolveUpdate()
-        terminal.println()
+        val transaction = archive.resolveUpdate()
 
-        val upSymbol = if (OperatingSystem.notWindows) "↑" else "u"
-
-        terminal.println("Updating the archive will result in the following transaction:")
-        updateResult.updateVersions.forEach {
-            terminal.println(TextColors.brightGreen(upSymbol) + " update " + it.optimalTerminalString())
-        }
-        updateResult.updateDependencyVersions.forEach {
-            terminal.println("${TextColors.brightGreen(upSymbol)} update ${it.optimalTerminalString()} ${TextColors.brightCyan("(dependency)")}")
-        }
-        updateResult.addedDependencyVersions.forEach {
-            terminal.println("${TextColors.brightGreen("+")} add ${it.optimalTerminalString()} ${TextColors.brightCyan("(dependency)")}")
-        }
-        updateResult.removedDependencies.forEach {
-            terminal.println("${TextColors.brightRed("-")} remove ${it.optimalTerminalString()} ${TextColors.brightCyan("(unused dependency)")}")
-        }
+        terminal.handleTransaction(
+            "Updating the archive will result in the following transaction:",
+            archive,
+            transaction
+        )
     }
 }
