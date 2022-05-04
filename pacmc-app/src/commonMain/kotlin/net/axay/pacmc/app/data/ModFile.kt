323b7c192bdc93bb968b1c8aa0689347da9dda4b
Jakob K <dev@jakobk.de>
2022-05-04 02:45:40 +0200

    Add modIdFromPath function to ModFile companion

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/data/ModFile.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/data/ModFile.kt
@@ -1,5 +1,7 @@
 package net.axay.pacmc.app.data
 
+import okio.Path
+
 class ModFile(
     val repositoryPart: String,
     val slugPart: String?,
@@ -7,4 +9,19 @@ class ModFile(
     val extension: String = "jar",
 ) {
     val fileName get() = "${slugPart?.plus("_").orEmpty()}${repositoryPart}_${idPart}.pacmc.${extension}"
+
+    companion object {
+        fun modIdFromPath(path: Path): ModId? {
+            val extension = path.name.takeLastWhile { it != '.' }
+            val fileName = path.name.removeSuffix(".pacmc.$extension")
+
+            val idPart = fileName.takeLastWhile { it != '_' }
+            val repositoryPart = fileName.removeSuffix("_${idPart}")
+
+            return ModId(
+                Repository.values().find { it.shortForm == repositoryPart } ?: return null,
+                idPart
+            )
+        }
+    }
 }
