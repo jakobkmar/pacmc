c74b79a0bb7cf220ae1594adecf00e3b9759c53d
Jakob K <dev@jakobk.de>
2022-05-08 21:40:21 +0200

    Provide special feedback if .minecraft is missing

--- pacmc-cli/src/commonMain/kotlin/net/axay/pacmc/cli/terminal/CliParser.kt
+++ pacmc-cli/src/commonMain/kotlin/net/axay/pacmc/cli/terminal/CliParser.kt
@@ -6,6 +6,7 @@ import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.defaultLazy
 import com.github.ajalt.clikt.parameters.options.defaultLazy
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.mordant.rendering.TextColors
 import net.axay.pacmc.app.features.Archive
 import net.axay.pacmc.cli.terminal
 
@@ -23,6 +24,9 @@ suspend fun Archive.Companion.fromString(name: String?): Archive? {
     val archive = Archive(name ?: getDefault())
     return if (archive.exists()) archive else {
         terminal.warning("The given archive '${archive.name}' does not exist")
+        if (archive.name == ".minecraft") {
+            terminal.warning("Try running ${TextColors.brightWhite("pacmc init")} to automatically detect the '.minecraft' folder.")
+        }
         null
     }
 }
