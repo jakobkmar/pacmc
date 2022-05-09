40b0b0156dcb0173375e5769476e654a4c0f7a78
Jakob K <dev@jakobk.de>
2022-05-10 00:30:06 +0200

    Create homebrew package (formulae)

--- packages/homebrew/pacmc.rb
+++ packages/homebrew/pacmc.rb
@@ -1,10 +1,16 @@
 class Pacmc < Formula
-    # TODO
-
-    desc "${description}"
-    homepage "${githubUrl}"
-
+    desc "An easy-to-use Minecraft package manager and launcher"
+    homepage "https://github.com/jakobkmar/pacmc"
+    url "https://github.com/jakobkmar/pacmc/releases/download/0.5.0/pacmc-0.5.0.tar"
+    sha256 "f90b5773a125e99ca10423be615f805337d394561cd25bbbf4d80f37aa3459fe"
+    license "AGPL-3.0-or-later"
+  
+    depends_on "openjdk"
+    
     def install
-
+      rm_f Dir["bin/*.bat"]
+      libexec.install %w[bin lib]
+      env = Language::Java.overridable_java_home_env
+      (bin/"pacmc").write_env_script libexec/"bin/pacmc", env
     end
-end
+  end
