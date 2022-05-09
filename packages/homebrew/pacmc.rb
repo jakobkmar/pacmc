53dbdd1a19dfcb3496a7b8d4a4341616cd195999
Jakob K <dev@jakobk.de>
2022-05-10 00:41:48 +0200

    Use shared package variables for homebrew template

--- packages/homebrew/pacmc.rb
+++ packages/homebrew/pacmc.rb
@@ -1,16 +1,16 @@
 class Pacmc < Formula
-    desc "An easy-to-use Minecraft package manager and launcher"
-    homepage "https://github.com/jakobkmar/pacmc"
-    url "https://github.com/jakobkmar/pacmc/releases/download/0.5.0/pacmc-0.5.0.tar"
-    sha256 "f90b5773a125e99ca10423be615f805337d394561cd25bbbf4d80f37aa3459fe"
-    license "AGPL-3.0-or-later"
-  
+    desc "Minecraft package manager and launcher"
+    homepage "${githubUrl}"
+    url "${githubUrl}/releases/download/${version}/pacmc-${version}.tar"
+    sha256 "${tarHashSha256}"
+    license "${license}"
+
     depends_on "openjdk"
-    
+
     def install
       rm_f Dir["bin/*.bat"]
       libexec.install %w[bin lib]
       env = Language::Java.overridable_java_home_env
       (bin/"pacmc").write_env_script libexec/"bin/pacmc", env
     end
-  end
+end
