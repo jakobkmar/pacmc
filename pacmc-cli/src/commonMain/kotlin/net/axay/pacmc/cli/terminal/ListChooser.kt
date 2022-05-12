67c539ebbcd83ef478bda4c93a550962e1327ebe
Jakob K <dev@jakobk.de>
2022-05-12 20:39:46 +0200

    Implement a basic list chooser
    Eventually I'd love to have a chooser which works with the arrow keys, but right now it is index based.

---
+++ pacmc-cli/src/commonMain/kotlin/net/axay/pacmc/cli/terminal/ListChooser.kt
@@ -0,0 +1,30 @@
+package net.axay.pacmc.cli.terminal
+
+import com.github.ajalt.mordant.terminal.Terminal
+
+fun <T> Terminal.choose(
+    message: String,
+    entries: List<Pair<T, String>>,
+): T? {
+    entries.forEachIndexed { index, (_, string) ->
+        print("${index + 1}) ")
+        println(string)
+    }
+    while (true) {
+        print("$message (provide the number): ")
+        val input = readlnOrNull() ?: return null
+
+        val index = input.toIntOrNull()?.minus(1)
+        if (index == null) {
+            warning("$input is not a valid number")
+            continue
+        }
+
+        if (index !in entries.indices) {
+            warning("$input is not a valid choice")
+            continue
+        }
+
+        return entries.getOrNull(index)?.first
+    }
+}
