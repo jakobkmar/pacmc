5d1966d096c74ce8de05e401abef91c2c3fe607b
Jakob K <dev@jakobk.de>
2022-05-13 20:21:29 +0200

    Add basic test to homebrew package

--- packages/homebrew/pacmc.rb
+++ packages/homebrew/pacmc.rb
@@ -1,16 +1,20 @@
 class Pacmc < Formula
-    desc "Minecraft package manager and launcher"
-    homepage "${githubUrl}"
-    url "${githubUrl}/releases/download/${version}/pacmc-${version}.tar"
-    sha256 "${tarHashSha256}"
-    license "${license}"
+  desc "Minecraft package manager and launcher"
+  homepage "${githubUrl}"
+  url "${githubUrl}/releases/download/${version}/pacmc-${version}.tar"
+  sha256 "${tarHashSha256}"
+  license "${license}"
 
-    depends_on "openjdk"
+  depends_on "openjdk"
 
-    def install
-      rm_f Dir["bin/*.bat"]
-      libexec.install %w[bin lib]
-      env = Language::Java.overridable_java_home_env
-      (bin/"pacmc").write_env_script libexec/"bin/pacmc", env
-    end
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin lib]
+    env = Language::Java.overridable_java_home_env
+    (bin/"pacmc").write_env_script libexec/"bin/pacmc", env
+  end
+
+  test do
+    assert_includes shell_output("#{bin}/pacmc debug test"), "Hello user! pacmc works."
+  end
 end
