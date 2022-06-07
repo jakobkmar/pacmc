74f9fa5b660637141575ce91559fdf176818be61
Jakob K <dev@jakobk.de>
2022-06-07 19:48:45 +0200

    Rule checks need the whole list context

--- pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/repoapi/mojang/RuleChecker.kt
+++ pacmc-app/src/commonMain/kotlin/net/axay/pacmc/app/repoapi/mojang/RuleChecker.kt
@@ -4,25 +4,48 @@ import co.touchlab.kermit.Logger
 import net.axay.pacmc.app.utils.OperatingSystem
 import net.axay.pacmc.repoapi.mojang.model.VersionPackage
 
-fun VersionPackage.Rule.check(): Boolean? {
-    val result = when (action) {
-        "allow" -> true
-        "disallow" -> false
-        else -> return null
-    }
+fun List<VersionPackage.Rule>?.check(): Boolean {
+    if (this == null) return true
+    if (this.isEmpty()) return true
 
-    os?.name?.let {
-        if (it != OperatingSystem.current?.mojangName)
-            return null
-    }
+    var allowed = false
 
-    features?.forEach { (feature, active) ->
-        when (feature) {
-            "is_demo_user" -> if (active) return null
-            "has_custom_resolution" -> { /* TODO */ }
-            else -> Logger.w("Unknown feature '$feature' in rule, skipping it")
+    for (rule in this) {
+        val result = when (rule.action) {
+            "allow" -> true
+            "disallow" -> false
+            else -> {
+                Logger.w("Unknown rule action '${rule.action}' in rule, skipping it")
+                continue
+            }
+        }
+
+        var doesApply = true
+
+        kotlin.run {
+            rule.os?.name?.let {
+                if (it != OperatingSystem.current?.mojangName) {
+                    doesApply = false
+                    return@run
+                }
+            }
+
+            rule.features?.forEach { (feature, active) ->
+                when (feature) {
+                    "is_demo_user" -> if (active) {
+                        doesApply = false
+                        return@run
+                    }
+                    "has_custom_resolution" -> { /* TODO */ }
+                    else -> Logger.w("Unknown feature '${feature}' in rule, skipping it")
+                }
+            }
+        }
+
+        if (doesApply) {
+            if (result) allowed = true else return false
         }
     }
 
-    return result
+    return allowed
 }
