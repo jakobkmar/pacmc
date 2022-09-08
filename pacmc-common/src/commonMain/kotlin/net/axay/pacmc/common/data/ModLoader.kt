5932a5d4fdc6ecb69d9beb16a58f2691d4c548d4
mooz <63669478+mooziii@users.noreply.github.com>
2022-09-08 19:08:31 +0200

    Support all loaders available on Modrinth

--- pacmc-common/src/commonMain/kotlin/net/axay/pacmc/common/data/ModLoader.kt
+++ pacmc-common/src/commonMain/kotlin/net/axay/pacmc/common/data/ModLoader.kt
@@ -3,10 +3,23 @@ package net.axay.pacmc.common.data
 enum class ModLoader(
     val displayName: String,
     val curseforgeId: Int?,
+    val compatibleLoaders: List<ModLoader> = emptyList(),
 ) {
     QUILT("Quilt", null),
     FABRIC("Fabric", 4),
     FORGE("Forge", 1);
+    FORGE("Forge", 1),
+    LITELOADER("LiteLoader", 3),
+    RIFT("Rift", null),
+    MODLOADER("Risugami's ModLoader", null),
+    BUKKIT("Bukkit", null),
+    SPIGOT("Spigot", null, listOf(BUKKIT)),
+    SPONGE("Sponge", null),
+    PAPER("Paper", null, listOf(BUKKIT, SPIGOT)),
+    PURPUR("Purpur", null, listOf(BUKKIT, SPIGOT, PAPER)),
+    BUNGEECORD("BungeeCord", null),
+    VELOCITY("Velocity", null),
+    WATERFALL("Waterfall", null, listOf(BUNGEECORD));
 
     val identifier = name.lowercase()
 }
